1c13ef2270cb43f06aa0eed8f18c2c2ef3b9d1f0
n_borovkov <n_borovkov@viyar.ua>
2024-01-10 12:01:00 +0200

    add tests and design

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,17 @@
 describe('template spec', () => {
-  it('passes', () => {
+  // it('passes', () => {
+  //   cy.visit('/')
+  //   cy.get('button').click()
+  //
+  //   cy.wait(1000)
+  //   cy.contains('h1', 'HELLO')
+  // })
+  it('should increase number', () => {
     cy.visit('/')
-    cy.get('button').click()
-
-    cy.wait(1000)
-    cy.contains('h1', 'HELLO')
-  })
+    cy.get('button[data-cy-btn="number"]').click()
+    cy.get('p[data-cy-number]').as('jopa')
+    cy.get('@jopa').should('contain', 1)
+    cy.get('button[data-cy-btn="number"]').click()
+    cy.get('@jopa').should('contain', 2)
+  });
 })
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 <template>
   <div>
-    <button @click="show">show</button>
+    <div>
+      <button data-cy-btn="number" @click="number++">Add</button>
+      <p data-cy-number>{{ number }}</p>
+    </div>
+    <br><br>
+    <button @click="showModal">show modal</button>
+    <br><br><br>
+    <button @click="showH1">show H1</button>
     <h1 v-if="isShow">HELLO</h1>
+    <modal @close="isShowModal = false" v-if="isShowModal"/>
   </div>
 </template>
 
 <script>
-
+import modal from './components/modal.vue'
 export default {
   name: 'App',
+  components: {
+    modal
+  },
   data() {
     return {
+      number: 0,
+      isShowModal: false,
       isShow: false
     }
   },
   methods: {
-    show() {
-      setTimeout(() => this.isShow = true, 1500)
+    showModal() {
+      this.isShowModal = true
+    },
+    showH1() {
+      this.isShow = !this.isShow
     }
   }
 }
